858ced743a5078132d2e219600b467490092bf85
firstsarcasm <som2e@exam2ple.com>
2022-02-17 10:05:18 +0300

    mascara-notifier-v2: --remove redundant cache 2

--- src/main/java/org/mascara/notifier/repository/SubscribersRepository.java
+++ src/main/java/org/mascara/notifier/repository/SubscribersRepository.java
@@ -1,7 +1,6 @@
 package org.mascara.notifier.repository;
 
 import org.mascara.notifier.entity.Subscriber;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
